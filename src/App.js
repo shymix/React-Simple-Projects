4d4c655be73716877c02bbe3e5d2acc015df5a06
shymix <sharevych@gmail.com>
2024-03-13 16:59:36 +0200

    minor bugs fixed

--- src/App.js
+++ src/App.js
@@ -9,22 +9,13 @@ import LoadMoreData from "./components/load-more-data";
 import ScrollToTopAndBottom from "./components/scroll-to-top-and-bottom";
 import ScrollToSection from "./components/scroll-to-top-and-bottom/scroll-to-section";
 
-import StarRating from "./components/star-rating";
-import TicTacToe from "./components/tic-tact-toe";
-import TreeView from "./components/tree-view";
-import menus from "./components/tree-view/data";
-
 import UseFetchHookTest from "./components/use-fetch/test";
-import UseWindowResizeTest from "./components/use-window-resize/test";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
       {/* <Accordian/> */}
-
-      {/* Star rating component */}
-      {/* <StarRating noOfStars={10}/> */}
 
       {/* Image slider component */}
       {/* <ImageSlider
@@ -36,17 +27,8 @@ function App() {
       {/* Load more products component */}
       {/* <LoadMoreData/> */}
 
-      {/* Tree view component/menu UI component / recursive navigation menu */}
-      {/* <TreeView menus={menus} /> */}
-
-      {/* Tic tac toe */}
-      {/* <TicTacToe/> */}
-
       {/* useFetch - Custom Hook Test*/}
       {/* <UseFetchHookTest/> */}
-
-      {/* Use Window Resize Hook Test */}
-      {/* <UseWindowResizeTest/> */}
 
       {/* Scroll to Top and Bottom */}
       {/* <ScrollToTopAndBottom/> */}
